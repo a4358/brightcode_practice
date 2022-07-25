110ebed8cdcedd8c95ca81dd6fa5d2a69de17dff
a4358 <you@example.com>
2022-07-25 20:15:14 +0300

    implemented full control logic, made a class

--- GameLogic.py
+++ GameLogic.py
@@ -1,8 +1,24 @@
 from Board import Board
 from BoardView import BoardView
+class GameLogic:
+    def __init__(self) -> None:
+        pass
+    def initialise(self,size):
+        if (type(size)!= int or size <=0):
+            raise ValueError
+        self.field = Board(size)
 
-field = Board(8)
-#field.make_move((4,3))
-#field.make_move((5,3))
-BoardView.draw_board(field)
-print(field.list_affected((4,3),-1))
+    def begin_game(self):
+
+        BoardView.draw_board(self.field)
+
+
+        while (self.field.moves_exist()):
+            try:
+                self.field.make_move(BoardView.prompt_move(self.field))
+            except: 
+                BoardView.invalid_move_comment()
+                continue
+            BoardView.draw_board(self.field)
+
+        BoardView.end_screen(self.field.report_pieces())
