171dd3f3232e1f171beb35c97f81e55f3d108ee4
a4358 <you@example.com>
2022-07-27 19:19:40 +0300

    fixed a critical bug that influenced ai decision making

--- AiPlayerEasy.py
+++ AiPlayerEasy.py
@@ -8,11 +8,12 @@ class AiPlayerEasy(Player):
         for x in range (board.size):
             for y in range (board.size):
                 if board.board[x][y] == 0:
-                    move_weight = board.list_affected((x,y), board.nextplayer)
+                    move_weight = len(board.list_affected((x,y), board.nextplayer))
                     if(move_weight):
                         xy = (x, y)
                         moves[xy] = move_weight
         bestmove = max (moves, key = moves.get)
         Player.currentplayer *= -1
+        print (f'The AI makes a move! expected value: {moves.get(bestmove)}')
         return (int(bestmove[0]),int(bestmove[1]))
        
